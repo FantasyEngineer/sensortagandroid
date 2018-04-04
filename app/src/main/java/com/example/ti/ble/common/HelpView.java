d61873d990c699ac52d0634d2f0c3f4c772ed7d1
houjiguo <jiguo.hou@dragonsoftbravo.com>
2018-04-04 18:30:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ti/ble/common/HelpView.java
@@ -0,0 +1,35 @@
+package com.example.ti.ble.common;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+public class HelpView extends Fragment {
+    private String mFile = "about.html";
+    private int mIdFragment;
+    private int mIdWebPage;
+
+    public HelpView() {
+
+    }
+
+    public void setParameters(String file, int idFragment, int idWebPage) {
+        if (file != null)
+            mFile = file;
+        mIdFragment = idFragment;
+        mIdWebPage = idWebPage;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(mIdFragment, container, false);
+        WebView wv = (WebView) rootView.findViewById(mIdWebPage);
+
+        wv.loadUrl("file:///android_asset/" + mFile);
+        return rootView;
+    }
+
+}
